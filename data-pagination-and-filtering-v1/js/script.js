68e6daff7a2ebeddc395c161baf8f467416fcc4e
jilliandike <43615050+jilliandike@users.noreply.github.com>
2024-08-25 10:19:19 -0600

    Updated comments

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -4,23 +4,20 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 
 
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
+//Declaring global variables that are used throughout the project.
 const studentsPerPage = 9;
+const studentList = document.querySelector(".student-list");
 
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
+/**
+ * This function creates the elements needed to display nine students and appends them to the page.
+ *
+ * @param {number} startIndex - Number to begin our index of students.
+ * @param {number} endIndex - Number that ends the index of students.
+ * @return {html} - Returns and inserts nine students on the page based on their index position.
+ */
 function showPage (list, page) {
    const startIndex = (page * studentsPerPage) - studentsPerPage;
    const endIndex = page * studentsPerPage;
-   const studentList = document.querySelector(".student-list");
    studentList.innerHTML = '';
    
    for (i = 0; i < list.length; i++) {
@@ -42,10 +39,14 @@ function showPage (list, page) {
    }
 }
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
+/**
+ * This function creates the elements needed to display pagination buttons and appends them to the page.
+ *
+ * @param {number} numberOfPages - Stores the number of pages needed.
+ * @param {string} linkList - Stores the ul element with class of link-list.
+ * @return {string} - Returns and inserts the necessary page number buttons, listens for them to be clicked, and applies 
+ * an active class to the one most recently clicked.
+ */
 function addPagination(list) {
    const numberOfPages = Math.ceil(list.length / studentsPerPage);
    const linkList = document.querySelector(".link-list");
@@ -74,15 +75,12 @@ function addPagination(list) {
    }
 )}
 
-
-// Call functions
+// Calls the functions described above to populate student data and page number buttons.
 showPage(data, 1);
 addPagination(data);
 
-
-//Adding Extra Credit sections
+//Inserts HTML for a search input field.
 const header = document.querySelector('header');
-
 header.insertAdjacentHTML('beforeend', 
    `
       <label for="search" class="student-search">
@@ -93,13 +91,17 @@ header.insertAdjacentHTML('beforeend',
    `
 );
 
-//Variables needed for searching
+//Declares the variables needed for searching and filtering student names.
 const studentSearchInput = document.getElementById('search');
 const studentSearchButton = document.querySelector('button[type=button]');
 const label = studentSearchInput.parentNode;  
-const studentList = document.querySelector(".student-list");
 
-
+/**
+ * This function searches through student names, filters, and returns any names that match the text entered in the search input.
+ *
+ * @param {array} newList - Stores names that match the search text.
+ * @return {html} - Returns and inserts matching students on the page and necessary page number buttons.
+ */
 function searchStudentData(input) {
    let newList = [];
    for (i = 0; i < data.length; i++) {
@@ -107,18 +109,19 @@ function searchStudentData(input) {
       let lastName = data[i].name.last.toLowerCase();
       let fullName = firstName + lastName;
          
-      if ( fullName.toLowerCase().includes(studentSearchInput.value.toLowerCase())) {
+      if (fullName.toLowerCase().includes(studentSearchInput.value.toLowerCase())) {
          newList.push(data[i]);
       } 
    }
    showPage(newList, 1);
    addPagination(newList);
    
-   if (newList.length === 0){
+   if (newList.length === 0) {
       studentList.innerHTML = `<h3>No results were found.</h3>`;
    }
 }
 
+//These event listeners are listening for keyboard input in the search field and any clicks on the search button.
 label.addEventListener('keyup', () => {
    searchStudentData(studentSearchInput.value);
 });
